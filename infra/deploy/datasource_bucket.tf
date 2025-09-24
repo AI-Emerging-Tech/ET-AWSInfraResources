615a6eccfc9c03bc063bf25038d42c43d032dc07
Dheeraj Bhupathiraju <dheerajvarma.bhupathiraju@valuemomentum.com>
2025-09-24 12:35:31 -0400

    consolidated IAM permissions to address quota limit and mapped env values for terraform apply

--- infra/deploy/datasource_bucket.tf
+++ infra/deploy/datasource_bucket.tf
@@ -12,28 +12,28 @@ resource "aws_s3_bucket" "data_source" {
 }
 
 
-resource "aws_vpc_endpoint" "s3_gateway" {
-  vpc_id       = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.region}.s3"
-  route_table_ids = [
-    aws_vpc.main.default_route_table_id
-  ]
-  vpc_endpoint_type = "Gateway"
+# resource "aws_vpc_endpoint" "s3_gateway" {
+#   vpc_id       = aws_vpc.main.id
+#   service_name = "com.amazonaws.${data.aws_region.current.region}.s3"
+#   route_table_ids = [
+#     aws_vpc.main.default_route_table_id
+#   ]
+#   vpc_endpoint_type = "Gateway"
 
-  policy = <<POLICY
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Action": "*",
-      "Effect": "Allow",
-      "Resource": "*",
-      "Principal": "*"
-    }
-  ]
-}
-POLICY
-}
+#   policy = <<POLICY
+# {
+#   "Version": "2008-10-17",
+#   "Statement": [
+#     {
+#       "Action": "*",
+#       "Effect": "Allow",
+#       "Resource": "*",
+#       "Principal": "*"
+#     }
+#   ]
+# }
+# POLICY
+# }
 
 resource "aws_s3_bucket_public_access_block" "data_source" {
   bucket                  = aws_s3_bucket.data_source.id
@@ -43,25 +43,25 @@ resource "aws_s3_bucket_public_access_block" "data_source" {
   restrict_public_buckets = true
 }
 
-resource "aws_vpc_endpoint" "s3_interface" {
-  depends_on          = [aws_vpc_endpoint.s3_gateway]
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.${data.aws_region.current.region}.s3"
-  private_dns_enabled = true
-  vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.rds.id]
+# resource "aws_vpc_endpoint" "s3_interface" {
+#   depends_on          = [aws_vpc_endpoint.s3_gateway]
+#   vpc_id              = aws_vpc.main.id
+#   service_name        = "com.amazonaws.${data.aws_region.current.region}.s3"
+#   private_dns_enabled = true
+#   vpc_endpoint_type   = "Interface"
+#   security_group_ids  = [aws_security_group.rds.id]
 
-  policy = <<POLICY
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Action": "*",
-      "Effect": "Allow",
-      "Resource": "*",
-      "Principal": "*"
-    }
-  ]
-}
-POLICY
-}
+#   policy = <<POLICY
+# {
+#   "Version": "2008-10-17",
+#   "Statement": [
+#     {
+#       "Action": "*",
+#       "Effect": "Allow",
+#       "Resource": "*",
+#       "Principal": "*"
+#     }
+#   ]
+# }
+# POLICY
+# }
