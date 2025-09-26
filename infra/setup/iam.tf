731f90ebd026566934fc4201107c93990da7bea7
Dheeraj Bhupathiraju <dheerajvarma.bhupathiraju@valuemomentum.com>
2025-09-26 12:04:15 -0400

    Modified CD user permissions for RDS modifysubnetgroup

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -214,7 +214,8 @@ data "aws_iam_policy_document" "cd_deploy" {
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
       "rds:ModifyDBInstance",
-      "rds:AddTagsToResource"
+      "rds:AddTagsToResource",
+      "rds:ModifyDBSubnetGroup"
     ]
     resources = ["*"]
   }
